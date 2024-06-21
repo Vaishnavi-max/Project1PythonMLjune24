9b93c06c9d227ba1a7e55938f29a3242146910a9
Vaishnavi-max <143889962+Vaishnavi-max@users.noreply.github.com>
2024-06-21 13:02:42 +0530

    Update A1q15.py

--- A1q15.py
+++ A1q15.py
@@ -1,6 +1,5 @@
 import csv
-file_path="C:/Users/vaish/OneDrive/Desktop/igdtuw ai ml internship/q15data.csv.txt"
-with open(file_path, mode='r') as file:
+with open("C:/Users/vaish/OneDrive/Desktop/igdtuw ai ml internship/secondfile.csv", mode='r') as file:
     csv_reader = csv.reader(file)
     for row in csv_reader:
         print(row)
